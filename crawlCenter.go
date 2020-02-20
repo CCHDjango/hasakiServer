e75fab6c1f0ebcc22a55c7ee16452451d65bc263
CCHDjango <30452675+CCHDjango@users.noreply.github.com>
2020-02-20 16:42:35 +0800

    update

--- crawlCenter.go
+++ crawlCenter.go
@@ -7,3 +7,34 @@ hasaki-quant server center的爬虫调度中心
 */
 package main
 
+type Crawl struct{
+
+}
+type crawlInterface interface{
+	setting(target string,saveMethod string)
+	startGovNewsCroll(saveMethod string,checkTime int)
+}
+
+func (c *Crawl) setting(target string,saveMethod string,checkTime int){
+	// function : 爬虫中心的设置，设置需要爬取的站点，保存方式,每次爬虫的轮询间隔
+	// param target : 需要爬取的对象站点
+	// param saveMethod : 保存数据的方式
+	// param checkTime : 爬虫轮询间隔
+
+	switch target {
+	case "govNewsCroll":
+		go_sync.Add(1)
+		go c.startGovNewsCroll(saveMethod,checkTime)
+		go_sync.Wait()
+	case "xinLangCaiJing":
+		break
+	}
+}
+
+func (c *Crawl) startGovNewsCroll(saveMethod string,checkTime int){
+	// function : 启动爬取中国政府官网滚动新闻
+	defer go_sync.Done()
+	for {
+		sleep(checkTime)
+	}
+}
