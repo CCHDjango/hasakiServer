194ccf41b5fc4c601e5f858663e053c6a4a8b38e
CCHDjango <30452675+CCHDjango@users.noreply.github.com>
2020-03-11 20:21:27 +0800

    update

--- gold.go
+++ gold.go
@@ -3,6 +3,10 @@
 黄金地址 : http://gu.sina.cn/m/?vt=1&cid=76613#/futures/foreign
 
 纽约黄金 : <li data-symbol="hf_GC"
+
+聚合数据的黄金数据接口API : http://web.juhe.cn:8080/finance/gold/shgold?key=您申请的APPKEY
+每两分钟更新一次，这个接口可能也是爬取别的站的，但是调用比直接爬取新浪财经快，
+作为备用方案
 */
 package main
 
